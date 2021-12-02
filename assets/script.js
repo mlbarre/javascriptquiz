5543f0f21647352fadd13faa147f05b059246a99
mlb <mlbarrera1538@gmail.com>
2021-12-02 01:36:17 -0600

    added questions plus start of timer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-var timerEl = document.getElementById('timer');
+var startBtn = document.querySelector(".start-btn");
+
+var timerEl = setInterval(function() {
+    if (timerEl > 0) {
+        document.getElementById("timer").innerHTML = " secs left";
+    } else (timerEl === 0) {
+        document.getElementById("timer").innerHTML = "Time's Up!";
+        clearInterval();
+    } 
+}, 1000);
+
+var startQuiz = function(event) {
+    // var firstQuestion = document.createElement("p");
+    // firstQuestion.innerHTML = "What is the use of setInterval()?"
+}
+
+startBtn.addEventListener("click", startQuiz);
